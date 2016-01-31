820ac3389a04ca2021306e85b314b61c8cbe0e97
Mansi <mpatel.y@gmail.com>
2016-01-30 22:05:22 -0500

    all pages

--- server.js
+++ server.js
@@ -3,15 +3,31 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var port = process.env.port || 3000;
+//home page
 app.get('/', function (req, res, next) {
     // res.send('hello express!') 
     //res.sendfile('./Public/index.html')
     res.sendfile(path.join(__dirname, "Public", "index.html"));
 });
-//info page
-app.get('/info', function (req, res) {
-    //res.send('custom info page...') 
-    res.sendfile(path.join(__dirname, "Public", "info.html"));
+//about page
+app.get('/about', function (req, res) {
+    //res.send('about page...') 
+    res.sendfile(path.join(__dirname, "Public", "about.html"));
+});
+//services page
+app.get('/services', function (req, res) {
+    //res.send('services  page...') 
+    res.sendfile(path.join(__dirname, "Public", "services.html"));
+});
+//projects page
+app.get('/projects', function (req, res) {
+    //res.send('projects page...') 
+    res.sendfile(path.join(__dirname, "Public", "projects.html"));
+});
+//contact me page 
+app.get('/contact_me', function (req, res) {
+    //res.send('contact me  page...') 
+    res.sendfile(path.join(__dirname, "Public", "contact_me.html"));
 });
 app.listen(port, function () {
     console.log("App started on port:" + port);
--- server.ts
+++ server.ts
@@ -6,18 +6,35 @@ var app:express.Express = express();
 
 var port:number = process.env.port || 3000;
 
+//home page
 app.get('/',function(req:express.Request, res:express.Response, next:any){
   // res.send('hello express!') 
   //res.sendfile('./Public/index.html')
   res.sendfile(path.join(__dirname,"Public","index.html"));
 });
 
-//info page
-app.get('/info',function(req:express.Request, res:express.Response){
-   //res.send('custom info page...') 
-   res.sendfile(path.join(__dirname,"Public","info.html"));
+//about page
+app.get('/about',function(req:express.Request, res:express.Response){
+   //res.send('about page...') 
+   res.sendfile(path.join(__dirname,"Public","about.html"));
 });
 
+//services page
+app.get('/services',function(req:express.Request, res:express.Response){
+   //res.send('services  page...') 
+   res.sendfile(path.join(__dirname,"Public","services.html"));
+});
+
+//projects page
+app.get('/projects',function(req:express.Request, res:express.Response){
+   //res.send('projects page...') 
+   res.sendfile(path.join(__dirname,"Public","projects.html"));
+});
+//contact me page 
+app.get('/contact_me',function(req:express.Request, res:express.Response){
+   //res.send('contact me  page...') 
+   res.sendfile(path.join(__dirname,"Public","contact_me.html"));
+});
 
 app.listen(port, function(){
     console.log("App started on port:" + port);
