da47e43d5901838c6ecb16011dfd8f1824086291
Mansi <mpatel.y@gmail.com>
2016-01-30 19:23:47 -0500

    running express

--- server.ts
+++ server.ts
@@ -1,5 +1,65 @@
 /// <reference path="_referance.ts" />
-import http =  require('http');
+import express = require('express');
+
+var app:express.Express = express();
+
+var port:number = process.env.port || 3000;
+
+app.get('/',function(req:express.Request, res:express.Response, next:any){
+   res.send('hello express!') 
+});
+
+//info page
+app.get('/info',function(req:express.Request, res:express.Response){
+   res.send('custom info page...') 
+});
+
+
+app.listen(port, function(){
+    console.log("App started on port:" + port);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import http =  require('http');
 
 var port:number = process.env.port || 3000;
 
@@ -13,3 +73,4 @@ var server:http.Server = http.createServer(
 server.listen(port,function(){
     console.log("Server Started... listening on port:" + port); 
 });
+*/
